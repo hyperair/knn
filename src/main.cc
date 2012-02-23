a794d6138e17ed828a87e1dda47e805c2a81ac53
Chow Loong Jin <hyperair@debian.org>
2012-02-23 11:07:00 +0800

    Add positional parameters

--- src/main.cc
+++ src/main.cc
@@ -6,10 +6,15 @@
 namespace bpo = boost::program_options;
 
 namespace {
-    template <typename T>
-    void store (const T source, T& dest)
+    void usage (const std::string program_name,
+                const bpo::options_description &desc)
     {
-        dest = source;
+        std::cerr << "Usage: " << program_name
+                  << " [options] <training-set-file> <test-set-file>"
+                  << std::endl << std::endl
+                  << desc << std::endl;
+
+        std::exit (1);
     }
 }
 
@@ -17,39 +22,63 @@ int main (int argc, char **argv)
 {
     using namespace std::placeholders;
 
-    int k;
-    std::string metric;
-
-    auto ksetter = std::bind (store<int>, _1, std::ref (k));
-    auto metricsetter = std::bind (store<std::string>, _1, std::ref (metric));
-
     bpo::options_description desc ("Allowed options");
     desc.add_options ()
         ("help,h", "Show this help message")
 
-        ("neighbours,k",
-         bpo::value<int> ()
-            ->notifier (ksetter)
-            ->default_value (1),
+        ("neighbours,k", bpo::value<int> () ->default_value (1),
          "Number of nearest neighbours to consider")
 
-        ("metric,d",
-         bpo::value<std::string> ()
-            ->notifier (metricsetter)
-            ->default_value ("euclidean"),
+        ("metric,d", bpo::value<std::string> () ->default_value ("euclidean"),
          "Difference metric (euclidean|cosine)");
 
+    bpo::options_description hidden;
+    hidden.add_options ()
+        ("training-set-file", bpo::value<std::string> (),
+         "Training set file")
+
+        ("test-set-file", bpo::value<std::string> (),
+         "Test set file");
+
+    bpo::options_description all_options;
+    all_options.add (desc).add (hidden);
+
+    bpo::positional_options_description positional_desc;
+    positional_desc.add ("training-set-file", 1);
+    positional_desc.add ("test-set-file", 1);
+
     bpo::variables_map vm;
-    bpo::store (bpo::parse_command_line (argc, argv, desc), vm);
+    bpo::store (bpo::command_line_parser (argc, argv)
+                    .options (all_options)
+                    .positional (positional_desc)
+                    .run (),
+                vm);
     bpo::notify (vm);
 
-    if (vm.count ("help")) {
-        std::cerr << desc << std::endl;
-        return 1;
-    }
 
+    // Check for proper usage
+    if (vm.count ("help") || !vm.count ("training-set-file") ||
+        !vm.count ("test-set-file"))
+        usage (argv[0], desc);
+
+
+    const std::string training_set_file =
+        vm["training-set-file"].as<std::string> ();
+
+    const std::string test_set_file =
+        vm["test-set-file"].as<std::string> ();
+
+    const int k = vm["neighbours"].as<int> ();
+    const std::string metric = vm["metric"].as<std::string> ();
+
+    if (metric != "euclidean" && metric != "cosine")
+        usage (argv[0], desc);
+
+    // TODO: replace this with actual logic
     std::cout << "k = " << k << std::endl
-              << "metric = " << metric << std::endl;
+              << "metric = " << metric << std::endl
+              << "training_set_file = " << training_set_file << std::endl
+              << "test_set_file = " << test_set_file << std::endl;
 
 
     return 0;
