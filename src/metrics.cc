96547e03a3688fd52665f34e4d4b8d063bb3315e
Chow Loong Jin <hyperair@debian.org>
2012-02-26 02:09:17 +0800

    Implement cosine similarity metric

--- src/metrics.cc
+++ src/metrics.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <metrics.hh>
 
 double knn::metrics::euclidean (const entry &e1, const entry &e2)
@@ -13,8 +14,20 @@ double knn::metrics::euclidean (const entry &e1, const entry &e2)
     return sq_sum;
 }
 
-double knn::metrics::cosine (const entry &, const entry &)
+double knn::metrics::cosine (const entry &e1, const entry &e2)
 {
-    // TODO: Implement
-    return double ();
+    entry::value_type numerator = 0;
+    entry::value_type sq_sum1 = 0;
+    entry::value_type sq_sum2 = 0;
+
+    e1.visit ([&] (const entry::index_type index, const entry::value_type value)
+              {
+                  const entry::value_type value2 = e2[index];
+
+                  numerator += value * value2;
+                  sq_sum1 += value * value;
+                  sq_sum2 += value2 * value2;
+              });
+
+    return double (numerator) / (std::sqrt (sq_sum1) * std::sqrt (sq_sum2));
 }
