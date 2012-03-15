688bb0a9143237678d9fcd3bec550fb4d95597b1
Chow Loong Jin <hyperair@debian.org>
2012-03-16 04:08:12 +0800

    Add mean and stdev generic functions

---
+++ src/stats.hh
@@ -0,0 +1,36 @@
+#ifndef STATS_HH
+#define STATS_HH
+
+#include <numeric>
+#include <iterator>
+#include <cmath>
+
+namespace knn
+{
+    namespace stats
+    {
+        template <typename Iterator>
+        double mean (Iterator begin, Iterator end)
+        {
+            auto sum = std::accumulate (begin, end, 0);
+
+            return double (sum) / std::distance (begin, end);
+        }
+
+        template <typename Iterator>
+        double stdev (Iterator begin, Iterator end, double mean)
+        {
+            double sqdiffsum = 0;
+
+            for (Iterator i = begin; i != end; ++i) {
+                double diff = *i - mean;
+
+                sqdiffsum += diff * diff;
+            }
+
+            return std::sqrt (sqdiffsum / std::distance (begin, end));
+        }
+    }
+}
+
+#endif  // STATS_HH
